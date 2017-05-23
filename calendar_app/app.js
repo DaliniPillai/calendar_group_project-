584a5e84b4a1c87f0519956231796ca1072ca509
Dalini <student@GAs-MacBook-Air.local>
2017-05-23 17:59:12 -0400

    added the routes to app.js

---
+++ calendar_app/app.js
@@ -0,0 +1,38 @@
+/* setting up express */
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+/* setting up port & listen */
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, function() {
+  console.log(`listening on port ${PORT}`);
+});
+
+/* setting static file */
+app.use('/static', express.static(path.join(__dirname, 'public')));
+/* setting up cors */
+app.use(cors());
+/* setting up logger */
+app.use(logger('dev'));
+/* setting up body parser */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+/* setting routes */
+/* ====================== INDEX ROUTE ========= */
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+/* tweeds API route */
+const tweedRoutes = require('./routes/calendarRoutes');
+app.use('/api/calendar', tweedRoutes);
+
+/* handling 404 */
+app.get('*', function(req, res) {
+  res.status(404).send({message: 'Oops! Not found.'});
+});
