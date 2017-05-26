1a7a52d120d777232fa402c8de13579eef5c50d9
robertprovine <robertprovine@gmail.com>
2017-05-26 01:14:55 -0400

    started working on Agenda View

---
+++ calendar_app/client/src/components/Agenda.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Agenda extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const currentDate = new Date();
+    return (
+      <div>
+        <h3>Agenda</h3>
+        {this.props.eventsData.map((event, i) => {
+          const timeStart = new Date(parseInt(event.time_start)).getHours();
+          const timeEnd = new Date(parseInt(event.time_end)).getHours();
+          const startAmPm = (timeStart < 12) ? 'AM' : 'PM';
+          const endAmPm = (timeEnd < 12) ? 'AM' : 'PM';
+          return (
+            <li key={i}>
+              <span>{(timeStart % 12) + ' ' + startAmPm}</span>
+              <span>{(timeEnd % 12) + ' ' + endAmPm}</span>
+              <span>{event.event_title}</span>
+            </li>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Agenda;
